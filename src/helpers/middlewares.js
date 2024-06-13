58b236250a2bf47efdec1ae9561c9c3a601c4c12
Cristian Requena <cristian91rp@gmail.com>
2024-06-13 18:35:32 +0200

    Corrección para mostrar grupos en el home

--- src/helpers/middlewares.js
+++ src/helpers/middlewares.js
@@ -2,9 +2,10 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET_KEY || "default_secret";
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
+  const token = req.headers["authorization"]?.split(' ')[1];
 
   if (!token) {
+    console.log("No se proporcionó un token.");
     return res.status(403).send({
       message: "No se proporcionó un token.",
     });
@@ -12,11 +13,13 @@ const verifyToken = (req, res, next) => {
 
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
+      console.log("Falló la autenticación del token:", err.message);
       return res.status(401).send({
         message: "Falló la autenticación del token.",
       });
     }
     req.userId = decoded.id;
+    console.log("Token verificado, userId:", req.userId);
     next();
   });
 };
